1208d654dae2df3a1f8e311418cdf04a610c7f92
Ash <ashlyhorner95@gmail.com>
2020-06-03 13:23:12 -0500

    removed test file

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.set('view engine','ejs');
+app.set('view engine','pug');
 app.use(express.static('public'));
 
 app.listen(3000);
+console.log('Listening on port: 3000');
