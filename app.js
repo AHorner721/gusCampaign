1b1a34bd88a92db67cf0cc82dabfdf2c64310744
Ash <ashlyhorner95@gmail.com>
2020-06-04 12:10:58 -0500

    modified donations section

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+if(process.env.NODE_ENV !== 'production'){
+    const env = require('dotenv');
+    env.config();
+}
+const stripe_skey = process.env.STRIPE_SKEY;
+const stripe_pubkey = process.env.STRIPE_PUBKEY;
+
 const express = require('express');
 const app = express();
 
-app.set('view engine','pug');
+app.set('view engine','ejs');
 app.use(express.static('public'));
 
 app.listen(3000);
 console.log('Listening on port: 3000');
+
+// Routes
+app.get('/', function(req,res){
+    res.sendFile(__dirname + '/index.html');
+});
