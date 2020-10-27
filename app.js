af2d5e0fa16043a90138f4ee7ee67cc398859916
Ash <ashlyhorner95@gmail.com>
2020-10-27 08:45:25 -0500

    modified form validation

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
+// check if in production mode
 if(process.env.NODE_ENV !== 'production'){
     const env = require('dotenv');
     env.config();
 }
+
+// API Key
 const stripe_skey = process.env.STRIPE_SKEY;
 
+// Import Express & Init Express App
 const express = require('express');
 const bodyparser = require('body-parser');
 const {body, validationResult} = require('express-validator');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Init View Engine
 app.set('view engine','ejs');
 app.use(express.static('public'));
 app.use(bodyparser.urlencoded({ extended: false }));
 
 // Routes
 app.get('/', (req,res)=>{
-    //res.sendFile(__dirname + '/index.html');
     res.render('index');
 });
 
@@ -25,18 +29,25 @@ app.get('/thanks', (req, res)=>{
 });
 
 app.post('/',[
-  body('_replyto').isEmail(),
-  body('name').isLength({min: 1, max: 50})
-  .withMessage('Name Empty').isAlpha().withMessage('Name must be letters').trim().escape(),
-  body('amount').toInt().isInt({min: 5, max: 1000})
-  .withMessage('Accepting Donations between $5 - $1000')
+  body('_replyto')
+    .isEmail()
+    .normalizeEmail(),
+  body('first')
+    .isLength({min: 3, max: 50}).withMessage('Name Length')
+    .isAlpha().withMessage('Name must be letters').trim().escape(),
+  body('last')
+    .isLength({min: 3, max: 50}).withMessage('Name Length')
+    .isAlpha().withMessage('Name must be letters').trim().escape(),
+  body('amount')
+    .toInt().isInt({min: 5, max: 1000})
+    .withMessage('Accepting Donations between $5 - $1000')
 ], async (req, res, next) => {
-    // TO ADD: data validation, storing errors in an `errors` variable!
+    // Check if request has any errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
     return res.status(422).json({ errors: errors.array() });
   }
-    const name = req.body.name;
+    const name =  `${req.body.first} ${req.body.last}`;
     const email = req.body._replyto;
     const amount = req.body.amount;
     console.log('Amount = ' + amount);
@@ -55,11 +66,13 @@ app.post('/',[
         console.log('Error! ', err.message);
       }
     }
+    next(); 
   });
 
 app.post('/charge', (req, res, next)=>{
   console.log('payment processing...');
-  res.render('thanks'); 
+  res.render('thanks');
+  next(); 
 });
 
 app.listen(port, ()=>{
